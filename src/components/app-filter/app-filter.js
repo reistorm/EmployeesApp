034147f8cbfb6b49644e806bf299ec85596632b5
Eliza X. <rysaeva96@mail.ru>
2026-03-26 14:40:22 +0300

    выполнена фильтрация и поиск

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,20 +1,41 @@
 import "./app-filter.css";
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+    const buttonsData = [
+        { name: 'all', label: 'Все сотрудники' },
+        { name: 'rise', label: 'На повышение' },
+        { name: 'moreThen1000', label: 'З/П больше 1000$' },
+    ];
+
+    const buttons = buttonsData.map(({ name, label }) => {
+        // eslint-disable-next-line no-undef
+        const active = props.filter === name;
+        const clazz = active ? 'btn-light' : 'btn-outline-light'
+        return (
+            <button type="button"
+                className={`btn ${clazz}`}
+                key={name}
+                onClick={() => props.onFilterSelect(name)}>
+                {label}
+            </button>
+        )
+    })
+
     return (
         <div className="btn-group">
-            <button type="button"
-                    className="btn btn-light">
-                    Все сотрудники
+            {buttons}
+            {/* <button type="button"
+                className="btn btn-light">
+                Все сотрудники
             </button>
             <button type="button"
-                    className="btn btn-outline-light">
-                    На повышение
+                className="btn btn-outline-light">
+                На повышение
             </button>
             <button type="button"
-                    className="btn btn-outline-light">
-                    З/П больше 1000$
-            </button>
+                className="btn btn-outline-light">
+                З/П больше 1000$
+            </button> */}
         </div>
     )
 }
